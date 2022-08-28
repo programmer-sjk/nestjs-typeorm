60c7eee3422a4789a453286bfe585dd2bbe5bf78
sjk5766 <wjdrnr6662@gmail.com>
2022-08-28 21:05:07 +0900

    DB 접속 테스트

--- src/module/app/app.service.ts
+++ src/module/app/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(
+    private manager: EntityManager
+  ) {}
+
+  async dbTest(): Promise<string> {
+    const result = await this.manager.query('show tables')
+    console.log(result)
     return 'Hello World!';
   }
 }

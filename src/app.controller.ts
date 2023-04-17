3aa00f49d917dd3dc9b6b26c0a601731371244fa
sjk5766 <wjdrnr6662@gmail.com>
2023-04-17 09:11:24 +0900

    feat: nestjs @Query, @Param, @Req, @Body 테스트

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateUserDto } from './CreateUserDto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/users')
+  getUserId(@Query('id') id): string {
+    return id;
+  }
+
+  @Get('/users/:id')
+  getUserIdByParam(@Param('id') id): string {
+    return id + 'param';
+  }
+
+  // @Post('/users')
+  // createUser(@Req() req: Request ): string {
+  //   console.log(req.body)
+  //   return 'ok';
+  // }
+
+  @Post('/users')
+  createUser(@Body() request: CreateUserDto ): string {
+    console.log(request)
+    return 'ok';
   }
 }

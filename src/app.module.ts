11a3ea8c591a58248f55a023e1dc70953d65280d
sjk5766 <wjdrnr6662@gmail.com>
2023-04-23 08:10:43 +0900

    feat: movie 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './user/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: false,
+    }),
+    UserModule,
+    MovieModule,
+  ],
   controllers: [],
   providers: [],
 })

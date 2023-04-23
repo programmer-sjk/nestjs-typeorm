854b06cc0e90a961bc3d1142e50f83f78a1a7b68
sjk5766 <wjdrnr6662@gmail.com>
2023-04-23 09:26:42 +0900

    feat: User 엔티티 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/users.module';
+import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MovieModule } from './movie/movie.module';
 

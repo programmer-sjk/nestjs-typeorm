66500aca62bd08fa8a14c0ea4d13ab4763200213
sjk5766 <wjdrnr6662@gmail.com>
2023-04-28 19:08:28 +0900

    feat: validation 추가

--- src/user/dto/CreateUserDto.ts
+++ src/user/dto/CreateUserDto.ts
@@ -1,8 +1,15 @@
+import { IsNotEmpty, MaxLength } from 'class-validator';
 import { User } from '../entity/User.entity';
 
 export class CreateUserDto {
+  @IsNotEmpty()
   private email: string;
+
+  @IsNotEmpty()
   private password: string;
+
+  @IsNotEmpty()
+  @MaxLength(10)
   private name: string;
 
   toEntity(): User {

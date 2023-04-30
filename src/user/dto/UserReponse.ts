82564ce369f269a38ccf7ecc56fbef3f8cf0b6e9
sjk5766 <wjdrnr6662@gmail.com>
2023-04-30 09:12:42 +0900

    feat: @Exclude, @Expose 설정으로 외부에서 _없이 볼수 있도록 수정

--- src/user/dto/UserReponse.ts
+++ src/user/dto/UserReponse.ts
@@ -1,10 +1,10 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 import { User } from '../entity/User.entity';
 
 export class UserResponse {
-  private readonly _id: number;
-  private readonly _email: string;
-  private readonly _name: string;
+  @Exclude() private readonly _id: number;
+  @Exclude() private readonly _email: string;
+  @Exclude() private readonly _name: string;
   @Exclude() private readonly _password: string;
 
   constructor(user: User) {
@@ -14,14 +14,17 @@ export class UserResponse {
     this._password = user.password;
   }
 
+  @Expose()
   public get id(): number {
     return this._id;
   }
 
+  @Expose()
   public get email(): string {
     return this._email;
   }
 
+  @Expose()
   public get name(): string {
     return this._name;
   }

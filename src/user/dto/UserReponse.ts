48a8b3c703a0df914bbc40210ed873d9c3bc555d
sjk5766 <wjdrnr6662@gmail.com>
2023-04-30 09:03:27 +0900

    feat: UserResponseDto getter 추가

--- src/user/dto/UserReponse.ts
+++ src/user/dto/UserReponse.ts
@@ -2,15 +2,27 @@ import { Exclude } from 'class-transformer';
 import { User } from '../entity/User.entity';
 
 export class UserResponse {
-  private readonly id: number;
-  private readonly email: string;
-  private readonly name: string;
-  @Exclude() private readonly password: string;
+  private readonly _id: number;
+  private readonly _email: string;
+  private readonly _name: string;
+  @Exclude() private readonly _password: string;
 
   constructor(user: User) {
-    this.id = user.id;
-    this.email = user.email;
-    this.name = user.name;
-    this.password = user.password;
+    this._id = user.id;
+    this._email = user.email;
+    this._name = user.name;
+    this._password = user.password;
+  }
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public get email(): string {
+    return this._email;
+  }
+
+  public get name(): string {
+    return this._name;
   }
 }

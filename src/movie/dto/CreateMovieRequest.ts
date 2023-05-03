bacde68d7ec9656b38ea6c9c0b0a0be76dd04597
sjk5766 <wjdrnr6662@gmail.com>
2023-05-04 08:47:46 +0900

    refactor: CreateMovieRequest validator 추가

--- src/movie/dto/CreateMovieRequest.ts
+++ src/movie/dto/CreateMovieRequest.ts
@@ -1,14 +1,32 @@
+import { IsInt, IsNotEmpty, Min } from 'class-validator';
 import { Movie } from '../entity/Movie.entity';
 import { SubTitleLanguage } from '../enum/SubTitleLanguage';
 
 export class CreateMovieRequest {
+  @IsNotEmpty()
   private title: string;
+
+  @IsNotEmpty()
   private subTitleLanguage: string;
+
+  @IsNotEmpty()
   private madeBy: string;
+
+  @IsNotEmpty()
   private fileCompany: string;
+
+  @IsInt()
+  @Min(0)
   private rottenScore: number;
+
+  @IsInt()
+  @Min(0)
   private imDbScore: number;
+
+  @IsInt()
+  @Min(0)
   private score: number;
+
   private description: string;
 
   toEntity(): Movie {
--- src/movie/dto/UpdateMovieScoreRequest.ts
+++ src/movie/dto/UpdateMovieScoreRequest.ts
@@ -1,5 +1,3 @@
-import { Movie } from '../entity/Movie.entity';
-
 export class UpdateMovieScoreRequest {
   rottenScore: number;
   imDbScore: number;

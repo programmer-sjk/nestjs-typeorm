905aa1d5eb6f4d46e3ec1b5a2dc2cf49d90094de
sjk5766 <wjdrnr6662@gmail.com>
2023-05-05 12:29:03 +0900

    refactor: 영화, 영화 평점 업데이트 dto에  validator 추가

--- src/movie/dto/UpdateMovieRequest.ts
+++ src/movie/dto/UpdateMovieRequest.ts
@@ -1,7 +1,18 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class UpdateMovieRequest {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   subTitleLanguage: string;
+  
+  @IsNotEmpty()
   madeBy: string;
+
+  @IsNotEmpty()
   filmCompany: string;
+
+  @IsNotEmpty()
   description: string;
 }
--- src/movie/dto/UpdateMovieScoreRequest.ts
+++ src/movie/dto/UpdateMovieScoreRequest.ts
@@ -1,5 +1,15 @@
+import { IsInt, Min } from 'class-validator';
+
 export class UpdateMovieScoreRequest {
+  @IsInt()
+  @Min(0)
   rottenScore: number;
+  
+  @IsInt()
+  @Min(0)
   imDbScore: number;
+  
+  @IsInt()
+  @Min(0)
   score: number;
 }

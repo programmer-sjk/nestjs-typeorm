f062a641f6795dc9d577ff8a2016b94b2aed6298
sjk5766 <wjdrnr6662@gmail.com>
2023-05-06 12:28:12 +0900

    feat: Review 등록/삭제 service layer 추가

--- src/review/entity/Review.entity.ts
+++ src/review/entity/Review.entity.ts
@@ -11,4 +11,9 @@ export class Review {
 
   @ManyToOne(() => Movie, (movie) => movie.reviews)
   movie: Movie;
+
+  constructor(content: string, movie: Movie) {
+    this.content = content;
+    this.movie = movie;
+  }
 }
---
+++ src/review/review.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ReviewRepository } from './review.repository';
+import { Review } from './entity/Review.entity';
+import { CreateReviewRequest } from './dto/CreateReviewRequest';
+import { MovieRepository } from '../movie/movie.repository';
+
+@Injectable()
+export class ReviewService {
+  constructor(
+    private readonly reviewRepository: ReviewRepository,
+    private readonly movieRepository: MovieRepository,
+  ) {}
+
+  async register(movieId: number, request: CreateReviewRequest): Promise<void> {
+    const movie = await this.movieRepository.findOneOrFail(movieId);
+    await this.reviewRepository.save(request.toEntity(movie));
+  }
+
+  async remove(id: number): Promise<void> {
+    const review = await this.findOne(id);
+    await this.reviewRepository.remove(review);
+  }
+
+  private async findOne(id: number): Promise<Review> {
+    return this.reviewRepository.findOne(id);
+  }
+}

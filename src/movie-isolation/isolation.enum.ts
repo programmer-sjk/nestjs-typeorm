57a34d39db46dc57f009ba784cea955f0edd6829
sjk5766 <wjdrnr6662@gmail.com>
2023-06-06 10:29:11 +0900

    feat: isolation 테스트 위한 enum 추가

---
+++ src/movie-isolation/isolation.enum.ts
@@ -0,0 +1,20 @@
+export enum RatingType {
+  ALL = 'ALL',
+  OVER12 = 'OVER12',
+  OVER15 = 'OVER15',
+  ADULT = 'ADULT',
+  RESTRICTED = 'RESTRICTED',
+  UNRATED = 'UNRATED',
+  ETC = 'ETC',
+}
+
+export enum LengthType {
+  LONG = 'LONG',
+  SHORT = 'SHORT',
+  ETC = 'ETC',
+}
+
+export enum MediaType {
+  MOVIE = 'MOVIE',
+  TV = 'DRAMA',
+}

2179d649b9833faa52a55deb88dbf97012bd7992
sjk5766 <wjdrnr6662@gmail.com>
2023-06-07 19:09:44 +0900

    feat: 테스트시 10만건 조회하도록 수정

--- src/movie-isolation/isolation.service.ts
+++ src/movie-isolation/isolation.service.ts
@@ -5,13 +5,13 @@ import { IsolationRepository } from './isolation.repository';
 export class IsolationService {
   constructor(private readonly isolationRepository: IsolationRepository) {}
 
-  async test(): Promise<void> {
-    await this.isolationRepository.find();
+  async test(id: number) {
+    return this.isolationRepository.findOne(id);
   }
 
   async getMovieIds() {
     return this.isolationRepository.query(
-      'SELECT idx as id FROM KLMovie ORDER BY RAND() LIMIT 100',
+      'SELECT idx as id FROM KLMovie ORDER BY RAND() LIMIT 100000',
     );
   }
 }

d2c3c73801c6c1250799886af8e25a2468323d16
sjk5766 <wjdrnr6662@gmail.com>
2023-06-07 19:05:08 +0900

    refactor: 잘못된 import 수정

--- src/movie-isolation/isolation.controller.ts
+++ src/movie-isolation/isolation.controller.ts
@@ -1,11 +1,11 @@
-import * as common from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { IsolationService } from './isolation.service';
 
-@common.Controller('isolation')
+@Controller('isolation')
 export class IsolationController {
   constructor(private readonly isolationService: IsolationService) {}
 
-  @common.Get()
+  @Get()
   async test(): Promise<void> {
     return await this.isolationService.test();
   }
